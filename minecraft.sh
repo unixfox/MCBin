9830ac6693ba6b0822b91f506292b1308501552a
Julien00859 <Julien00859@gmail.com>
2014-12-02 15:09:40 +0100

    Ajout de l'argument exec qui permet de lancer une commande dans la console du serveur

--- minecraft.sh
+++ minecraft.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 #Création par unixfox (Emilien) - Created by unixfox
 
-MC_PATH=/le/répertoire/où/se/trouve/votre/serveur/ #Répertoire de votre serveur minecraft - Directory of your Minecraft server
-NOM_JAR="minecraft_server.jar" # Nom du fichier .jar de votre serveur minecraft - Name of the .jar file of your Minecraft server
+MC_PATH=/home/julien/Public/Minecraft/ #Répertoire de votre serveur minecraft - Directory of your Minecraft server
+NOM_JAR="craftbukkit.jar" # Nom du fichier .jar de votre serveur minecraft - Name of the .jar file of your Minecraft server
 MEMALOC=512 # Mémoire à allouer à votre serveur minecraft - Memory of your Minecraft Server
 
 server_stop() {
@@ -12,7 +12,7 @@ server_stop() {
 case "$1" in
   start)
         echo -n "Lancement du serveur minecraft"
-        cd $MC_SCRIPT; screen -S minecraft java -jar -Xmx${MEMALOC}M -Xms512M -XX:MaxPermSize=128M -Dfile.encoding=UTF8 $NOM_JAR
+        cd $MC_PATH; screen -S minecraft java -jar -Xmx${MEMALOC}M -Xms512M -XX:MaxPermSize=128M -Dfile.encoding=UTF8 $NOM_JAR
         echo "."
         ;;
   stop)
@@ -20,8 +20,15 @@ case "$1" in
         server_stop
         echo "."
         ;;
-
+  exec)
+	if [ -z $2 ]; then
+		echo -n "Utilisation: service minecraft exec <command>"
+	else
+		msg=${@#exec }
+		screen -S minecraft -p 0 -X stuff "`printf "$msg\r"`"
+	fi
+	;;
   *)
-        echo "Utilisation: service minecraft {start|stop}"
+        echo "Utilisation: service minecraft {start|stop|exec}"
         exit 1
 esac
