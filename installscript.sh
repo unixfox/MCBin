10cf5885cb36db1dc6fa658b8cf5f321c2c571cf
unixfox <mimidevos@live.be>
2014-12-03 16:05:19 +0100

    Script final

--- installscript.sh
+++ installscript.sh
@@ -1,18 +1,8 @@
 #! /bin/sh
 
-autoinstall() {
-  sudo wget https://github.com/unixfox/Minecraft-Server-Linux-service/releases/download/2.0/minecraft.sh -P /tmp
-  sudo mv /tmp/minecraft.sh /etc/init.d/minecraft
-  sudo chmod a+x /etc/init.d/minecraft
-}
+#Script d'installation automatique du service Minecraft
 
-case "$1" in
-  startup)
-    autoinstall
-    sleep 1
-		sudo update-rc.d minecraft defaults
-		;;
-  *)
-		autoinstall
-		exit 1
-esac
+sudo wget https://github.com/unixfox/Minecraft-Server-Linux-service/releases/download/2.0/minecraft -P /etc/init.d/
+sudo chmod a+x /etc/init.d/minecraft
+sudo nano /etc/init.d/minecraft
+sudo update-rc.d minecraft defaults
