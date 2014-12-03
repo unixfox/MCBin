1974baad6be129fdf9602a3b9bd9f34faebc01cc
Emilien Devos <unixfox@users.noreply.github.com>
2014-12-03 15:12:22 +0100

    Ajout de commentaires
    Pour aider l'utilisateur.

--- minecraft.sh
+++ minecraft.sh
@@ -2,15 +2,18 @@
 #Création par unixfox (Emilien) - Created by unixfox
 #Merci Julien008 pour les suggestions et report de bugs.
 
+#Descriptions du service
 DESC="Script permettant de gérer un serveur Minecraft à partir d'un service."
 SERVICE_NAME=minecraft
 
+#Options
 MC_PATH='/le/répertoire/où/se/trouve/votre/serveur/' #Répertoire de votre serveur minecraft - Directory of your Minecraft server
-NOM_JAR='minecraft_server.jar' # Nom du fichier .jar de votre serveur minecraft - Name of the .jar file of your Minecraft server
-MEMALOC=512 # Mémoire à allouer à votre serveur minecraft - Memory of your Minecraft Server
+NOM_JAR='minecraft_server.jar' #Nom du fichier .jar de votre serveur minecraft - Name of the .jar file of your Minecraft server
+MEMALOC=512 #Mémoire à allouer à votre serveur minecraft - Memory of your Minecraft Server
 TPSWARN=10 #Temps après le quel le serveur va s'éteindre ou redémarrer. Argument warn à ajouter à la commande stop ou restart.
 SCREEN_NAME='minecraft' #Nom de la fenêtre.
 
+#Variables
 server_stop() {
         echo -n "Arrêt du serveur Minecraft..."
         screen -S $SCREEN_NAME -p 0 -X stuff "`printf "say Arrêt du serveur dans $TPSWARN SECONDES.\r"`"
@@ -22,7 +25,7 @@ server_stop() {
 }
 
 server_start() {
-  echo -n "Lancement du serveur minecraft..."
+        echo -n "Lancement du serveur minecraft..."
         cd $MC_PATH && screen -h 1024 -dmS $SCREEN_NAME java -jar -Xmx${MEMALOC}M -Xms512M -XX:MaxPermSize=128M -Dfile.encoding=UTF8 $NOM_JAR nogui; 
         sleep 1
         echo " [OK]"
@@ -37,9 +40,10 @@ commande() {
 }
 
 console() {
-  screen -r $SCREEN_NAME
+     screen -r $SCREEN_NAME
 }
 
+# Corps du script
 case "$1" in
   start)
         server_start
