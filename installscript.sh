4bf2a4004d72d0a7747e60360c93e8db0c9753f7
unixfox <mimidevos@live.be>
2014-12-03 15:52:16 +0100

    Quelques changements

--- installscript.sh
+++ installscript.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 autoinstall() {
-  sudo wget https://github.com/unixfox/Minecraft-Server-Linux-service/releases/download/2.0/minecraft.sh -p /etc/init.d/
+  sudo wget https://github.com/unixfox/Minecraft-Server-Linux-service/releases/download/2.0/minecraft.sh -P /tmp
+  sudo mv /tmp/minecraft.sh /etc/init.d/minecraft
   sudo chmod a+x /etc/init.d/minecraft
 }
 
