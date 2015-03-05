ce4c4e49bc9bfad3b23593bb17c41381978f6936
Emilien Devos <unixfox@users.noreply.github.com>
2015-03-05 22:33:46 +0100

    Remove stupid space

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
-
 #Script d'installation automatique du service Minecraft
-
 sudo wget https://github.com/unixfox/Minecraft-Server-Linux-service/releases/download/2.0/minecraft -P /etc/init.d/
 sudo chmod a+x /etc/init.d/minecraft
 sudo nano /etc/init.d/minecraft
